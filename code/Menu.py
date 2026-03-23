a4c8d8193ce69df675c176bb556dcd11013564fa
DaniloDev <daniloemanuel684@gmail.com>
2026-03-23 13:54:51 -0300

    Adding Music for Level1 Setting the Menu and Level Volume Creating some info texts in Level Screen

--- code/Menu.py
+++ code/Menu.py
@@ -9,12 +9,13 @@ from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load("./asset/MenuBg.png")
+        self.surf = pygame.image.load("./asset/MenuBg.png").convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
         menu_option = 0
         pygame.mixer_music.load("./asset/Menu.mp3")
+        pygame.mixer_music.set_volume(0.5)
         pygame.mixer_music.play(-1)
         while True:
             # DRAW IMAGES
