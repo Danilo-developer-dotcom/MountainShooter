d31fe8c295b5b27ad3d90b60159d1ecbe633afc9
DaniloDev <daniloemanuel684@gmail.com>
2026-03-30 14:24:19 -0300

    Fixing one background bug

--- code/Const.py
+++ code/Const.py
@@ -14,7 +14,7 @@ ENTITY_SPEED = {
     'Level1Bg2' : 2,
     'Level1Bg3' : 3,
     'Level1Bg4' : 4,
-    'Level1Bg5' : 5,
+    'Level1Bg5' : 4,
     'Level1Bg6' : 6,
     'Player1' : 3,
     'Player1Shot': 5,
